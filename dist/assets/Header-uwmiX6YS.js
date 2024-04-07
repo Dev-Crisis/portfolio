df4e810564763ea1c556d2010331e142b0e1721d
Kevin Bardou <kbardou86@gmail.com>
2024-04-07 17:23:32 +0200

    adding dist

---
+++ dist/assets/Header-uwmiX6YS.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/NavBar-RLFqvDKg.js","assets/index-j-krwaZ6.js","assets/index-BGZy_C6i.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{b as a,e as r,k as t,u as _,s as o,_ as n}from"./index-j-krwaZ6.js";const d={__name:"Header",setup(s){const e=o(()=>n(()=>import("./NavBar-RLFqvDKg.js"),__vite__mapDeps([0,1,2])));return(c,p)=>(a(),r("header",null,[t(_(e))]))}};export{d as default};
