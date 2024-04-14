ac0219006c4e0ccaacfef178a71988b23ae41273
Kevin Bardou <kbardou86@gmail.com>
2024-04-14 19:29:15 +0200

    fix text error projects -> projet

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -4,7 +4,7 @@
     <section class="flex justify-center items-center flex-col gap-5 py-5 w-full">
       <div class="flex flex-col gap-3 justify-center items-start">
         <h1 class="text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl text-black uppercase text-shadow-black dark:text-shadow-white">
-          Projects</h1>
+          Projets</h1>
       </div>
       <div class="w-5/6 md:w-2/5">
         <Vue3Lottie no-margin :animationData="Projects"/>
