d7dd5520fd5405c43bfd3f75ddbf6d77a63363f9
Kevin Bardou <kbardou86@gmail.com>
2024-04-14 17:58:35 +0200

    adding dist

--- src/components/NotFound.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <RouterLink title="Retourner à l'accueil" to="/" class="uppercase text-lg font-bold" active-class="underline">
-    <Vue3Lottie no-margin :animationData="NotFound" @click.passive=""/>
-  </RouterLink>
-</template>
-<script setup lang="ts">
-import {Vue3Lottie} from "vue3-lottie";
-import NotFound from "../assets/animations/404.json";
-</script>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="p-4 flex ">
+    <h1 class="text-4xl 2xl:text-5xl uppercase text-center">A propos</h1>
+  </section>
+  <!-- TODO: My student path  -->
+  <section>
+    <!--    <h2 class="text-2xl uppercase">My Student path</h2>-->
+  </section>
+  <!-- TODO: LIDEM -->
+  <section>
+    <!--    <h2 class="text-2xl uppercase">LIDEM</h2>-->
+  </section>
+  <!-- TODO: My hobbies -->
+  <section>
+    <!--    <h2 class="text-2xl uppercase">My Hobbies</h2>-->
+  </section>
+</template>
