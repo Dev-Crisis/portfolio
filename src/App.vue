32d094afe255cc3a91a5db2e3fe39d06ee3ed8c1
Kevin Bardou <kbardou86@gmail.com>
2024-06-06 21:14:15 +0200

    New Version

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Header/>
+  <HeroBanner/>
+  <Skills/>
+  <Projects/>
+  <About/>
+</template>
+<script>
+import Header from "./components/Header.vue";
+import HeroBanner from "./components/HeroBanner.vue";
+import Skills from "./components/Skills.vue";
+import Projects from "./components/Projects.vue";
+import About from "./components/About.vue";
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  components: {Header, Skills, HeroBanner, Projects, About}
+})
+
+</script>
